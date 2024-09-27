4c90fb09f8f6c707e15c2eb9e77c6fc921f3efdb
JYCruz <josh.cruz@neu.edu.ph>
2024-09-27 10:46:55 +0800

    Update AirConditioning.java

--- AirConditioning.java
+++ AirConditioning.java
@@ -1,1 +1,12 @@
+package Java;
+
+public class AirConditioning implements HomeService{
+    public void turnOn(){
+        System.out.println("Air Conditioning on!");
+    }
+
+    public void turnOff(){
+        System.out.println("Air Conditioning off!");
+    }
+}
 
