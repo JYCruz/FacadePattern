8eabcf4a4f2560d63a6743368d65c56e9d1839f6
JYCruz <josh.cruz@neu.edu.ph>
2024-09-27 10:32:55 +0800

    Update HomeApp.java

--- HomeApp.java
+++ HomeApp.java
@@ -1,1 +1,18 @@
+package Java;
 
+public class HomeApp{
+    public static void main(String[] args){
+        HomeService lights = new Lights();
+        HomeService tv = new Tv();
+        HomeService airConditioning = new AirConditioning();
+
+        HomeInterface homeInterface = new HomeInterface(lights, tv, airConditioning);
+
+        System.out.println("Turn on all---------------");
+        homeInterface.turnOnAll();
+        System.out.println("");
+        System.out.println("Turn off all---------------");
+        homeInterface.turnOffAll();
+
+    }
+}
