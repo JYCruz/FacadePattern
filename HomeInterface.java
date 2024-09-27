60c11eae9b851d171d2dbc14c720f953f6cae56a
JYCruz <josh.cruz@neu.edu.ph>
2024-09-27 10:33:21 +0800

    Update HomeInterface.java

--- HomeInterface.java
+++ HomeInterface.java
@@ -1,1 +1,27 @@
+package Java;
+
+public class HomeInterface{
+    private HomeService lights;
+    private HomeService tv;
+    private HomeService airConditioning;
+
+    public HomeInterface(HomeService lights, HomeService tv, HomeService airConditioning2){
+        this.lights = lights;
+        this.tv = tv;
+        this.airConditioning = airConditioning2;
+
+    }
+
+    public void turnOnAll(){
+        lights.turnOn();
+        tv.turnOn();
+        airConditioning.turnOn();
+    }
+
+    public void turnOffAll(){
+        lights.turnOff();
+        tv.turnOff();
+        airConditioning.turnOff();
+    }
+}
 
